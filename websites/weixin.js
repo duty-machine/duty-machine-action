3a2559d34c4e0477501f6f3862e096fbe8ff8558
duty-machine <63031384+duty-machine@users.noreply.github.com>
2020-12-03 09:21:55 -0400

    support non-activity article on weixin

--- websites/weixin.js
+++ websites/weixin.js
@@ -29,16 +29,31 @@ module.exports = {
     let html = await res.text()
     let document = new JSDOM(html).window.document
 
-    let title = document.querySelector('#activity-name').textContent
-    let author = document.querySelector('#js_name').textContent
-    let content = document.querySelector('#js_content')
-    let publishTime = this.getPublishTime(document)
+    let title, author, content, publishTime
+    // two types of weixin article
+    if (document.querySelector('#activity-detail')) {
+      title = document.querySelector('#activity-name').textContent
+      author = document.querySelector('#js_name').textContent
+      content = document.querySelector('#js_content')
+      publishTime = this.getPublishTime(document)
 
-    Array.from(content.querySelectorAll('img')).map(img => {
-      if (img.dataset.src) {
-        img.src = img.dataset.src
-      }
-    })
+      Array.from(content.querySelectorAll('img')).map(img => {
+        if (img.dataset.src) {
+          img.src = img.dataset.src
+        }
+      })
+    } else {
+      title = document.querySelector('meta[property="og:title"]').content
+      author = document.querySelector('.account_nickname_inner').textContent
+      content = document.querySelector('#js_content')
+      publishTime = this.getPublishTime(document)
+
+      Array.from(content.querySelectorAll('img')).map(img => {
+        if (img.dataset.src) {
+          img.src = img.dataset.src
+        }
+      })
+    }
 
     return {
       title,
@@ -51,7 +66,8 @@ module.exports = {
 
   samples: [
     'http://mp.weixin.qq.com/s?__biz=Mzg4OTE2MzU1Ng==&amp;mid=2247486095&amp;idx=1&amp;sn=98a427fc595b42cd804fd6ca8992f673&amp;chksm=cff150c4f886d9d2b120b42379d1e4255d2f6e9bfbac7afc7a9c3fc712540220e6484d48c286&amp;mpshare=1&amp;scene=1&amp;srcid=&amp;sharer_sharetime=1586605001116&amp;sharer_shareid=f467668849c8544e583567bf8a259f31#rd',
-    'https://mp.weixin.qq.com/s/xDKKicV22IBRGnNnNStOVg'
+    'https://mp.weixin.qq.com/s/xDKKicV22IBRGnNnNStOVg',
+    'https://mp.weixin.qq.com/s/5HyoGdzQ4YJfHCzdwJTF_Q'
     //'https://wechatscope.jmsc.hku.hk/api/html?fn=gh_3c3083a27d8e_2020-09-29_2247507405_Wi6OnYd2ak.y.tar.gz'
   ]
 
